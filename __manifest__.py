67cb27c4465cbcc88c47e9a990cbfcf4f8a7aa08
jusiho <pierreblas123@gmail.com>
2023-10-03 13:34:28 -0500

    cambios para bien

--- __manifest__.py
+++ __manifest__.py
@@ -59,66 +59,64 @@
     'assets': {
         'web.assets_backend': [
 
-            # 'gastos_tqc/static/src/views/*.js',
-            # 'gastos_tqc/static/src/**/*.xml',
-            # 'gastos_tqc/static/src/js/liquidacion_header.js',
-            # 'gastos_tqc/static/src/js/registro_gasto.js',
-            'gastos_tqc/static/src/scss/gastos_tqc.scss',
-            'gastos_tqc/static/src/scss/table_liqui.scss',
-            'gastos_tqc/static/src/scss/modal_style.scss',
-
-            # split view
-            'gastos_tqc/static/src/views/split_view/side_form_view.scss',
-
-            # 'gastos_tqc/static/src/views/split_view/x2many_field.xml',
-
-            'gastos_tqc/static/src/views/split_view/side_form_status_indicator.xml',
-            'gastos_tqc/static/src/views/split_view/side_form_control_panel.xml',
-            'gastos_tqc/static/src/views/split_view/side_form_controller.xml',
-            'gastos_tqc/static/src/views/split_view/side_form_view_container.xml',
-
-            # 'gastos_tqc/static/src/views/split_view/list_controller.xml',
-            'gastos_tqc/static/src/views/split_view/list_renderer.xml',
-
-            # views header flujo
-            'gastos_tqc/static/src/views/header_flujo.xml',
-            'gastos_tqc/static/src/views/hide_icon_delete.xml',
-            'gastos_tqc/static/src/views/one2many_selectable.xml',
-
-            # views js
-            'gastos_tqc/static/src/views/header_flujo.js',
-            'gastos_tqc/static/src/views/header_registry.js',
-            'gastos_tqc/static/src/views/hide_icon_delete.js',
-            'gastos_tqc/static/src/views/one2many_selectable.js',
-
-            # INTERACT
-            'gastos_tqc/static/src/lib/interact.min.js',
-
-            'gastos_tqc/static/src/views/split_view/hooks.js',
-
-            'gastos_tqc/static/src/views/split_view/with_search.js',
-
-            'gastos_tqc/static/src/views/split_view/form_view.js',
-            'gastos_tqc/static/src/views/split_view/side_form_status_indicator.js',
-            'gastos_tqc/static/src/views/split_view/side_form_control_panel.js',
-            'gastos_tqc/static/src/views/split_view/side_form_controller.js',
-            'gastos_tqc/static/src/views/split_view/side_form_view_container.js',
-
-            'gastos_tqc/static/src/views/split_view/list_arch_parser.js',
-            # 'gastos_tqc/static/src/views/split_view/list_controller.js',
-            'gastos_tqc/static/src/views/split_view/list_renderer.js',
-
-
-
-            # 'gastos_tqc/static/src/js/modal_search.js',
-            'gastos_tqc/static/src/js/clicked_field.js',
-            'gastos_tqc/static/src/js/wiget_text.js',
-            'gastos_tqc/static/src/js/render_search_ruc.js',
-            'gastos_tqc/static/src/js/render_search_client.js',
-            'gastos_tqc/static/src/js/get_cuenta_contable.js',
-
-            'gastos_tqc/static/src/js/list_renderer.js',
-            # 'gastos_tqc/static/src/js/web_client.js',
+            # # 'gastos_tqc/static/src/views/*.js',
+            # # 'gastos_tqc/static/src/**/*.xml',
+            # # 'gastos_tqc/static/src/js/liquidacion_header.js',
+            # # 'gastos_tqc/static/src/js/registro_gasto.js',
+            # 'gastos_tqc/static/src/scss/gastos_tqc.scss',
+            # 'gastos_tqc/static/src/scss/table_liqui.scss',
+            # 'gastos_tqc/static/src/scss/modal_style.scss',
+            #
+            # # split view
+            # 'gastos_tqc/static/src/views/split_view/side_form_view.scss',
+            #
+            # # 'gastos_tqc/static/src/views/split_view/x2many_field.xml',
+            #
+            # 'gastos_tqc/static/src/views/split_view/side_form_status_indicator.xml',
+            # 'gastos_tqc/static/src/views/split_view/side_form_control_panel.xml',
+            # 'gastos_tqc/static/src/views/split_view/side_form_controller.xml',
+            # 'gastos_tqc/static/src/views/split_view/side_form_view_container.xml',
+            #
+            # # 'gastos_tqc/static/src/views/split_view/list_controller.xml',
+            # 'gastos_tqc/static/src/views/split_view/list_renderer.xml',
+            #
+            # # views header flujo
+            # 'gastos_tqc/static/src/views/header_flujo.xml',
+            # 'gastos_tqc/static/src/views/hide_icon_delete.xml',
+            # 'gastos_tqc/static/src/views/one2many_selectable.xml',
+            #
+            # # views js
+            # 'gastos_tqc/static/src/views/header_flujo.js',
+            # 'gastos_tqc/static/src/views/header_registry.js',
+            # 'gastos_tqc/static/src/views/hide_icon_delete.js',
+            # 'gastos_tqc/static/src/views/one2many_selectable.js',
+            #
+            # # INTERACT
+            # 'gastos_tqc/static/src/lib/interact.min.js',
+            #
+            # 'gastos_tqc/static/src/views/split_view/hooks.js',
+            #
+            # 'gastos_tqc/static/src/views/split_view/with_search.js',
+            #
+            # 'gastos_tqc/static/src/views/split_view/form_view.js',
+            # 'gastos_tqc/static/src/views/split_view/side_form_status_indicator.js',
+            # 'gastos_tqc/static/src/views/split_view/side_form_control_panel.js',
+            # 'gastos_tqc/static/src/views/split_view/side_form_controller.js',
+            # 'gastos_tqc/static/src/views/split_view/side_form_view_container.js',
+            #
+            # 'gastos_tqc/static/src/views/split_view/list_arch_parser.js',
+            # # 'gastos_tqc/static/src/views/split_view/list_controller.js',
+            # 'gastos_tqc/static/src/views/split_view/list_renderer.js',
+            #
+            # # 'gastos_tqc/static/src/js/modal_search.js',
+            # # 'gastos_tqc/static/src/js/clicked_field.js',
+            # 'gastos_tqc/static/src/js/wiget_text.js',
+            # # 'gastos_tqc/static/src/js/render_search_ruc.js',
+            # # 'gastos_tqc/static/src/js/render_search_client.js',
+            # # 'gastos_tqc/static/src/js/get_cuenta_contable.js',
+            #
+            # # 'gastos_tqc/static/src/js/list_renderer.js',
+            # # 'gastos_tqc/static/src/js/web_client.js',
         ],
         # 'web.assets_qweb': [
         #     'gastos_tqc/static/src/xml/liquidacion_header.xml',
