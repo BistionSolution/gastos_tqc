99244c6553727bb1f509ebf9ebd96a4dc851cbf8
jusiho <pierreblas123@gmail.com>
2024-08-05 12:40:02 -0500

    go changes fix

--- models/tqc_liquidaciones.py
+++ models/tqc_liquidaciones.py
@@ -450,8 +450,8 @@ class Liquidaciones(models.Model):
                             variJsonNew['habilitado_state'] = 'liquidado'
 
                         variJsonNew['saldo'] = user[7]
-                        variJsonNew['empleado_name'] = self.env['hr.employee'].sudo().search(
-                            [('id_integrador', '=', user[2])]).id
+                        # variJsonNew['empleado_name'] = self.env['hr.employee'].sudo().search(
+                        #     [('id_integrador', '=', user[2])]).id
                         self.env[table_bd].browse(id_register).sudo().write(variJsonNew)
                         self.env.cr.commit()
 
