574b8e98cc6201f8218143894217265c1e499a9c
jusiho <pierreblas123@gmail.com>
2024-08-19 17:04:24 -0500

    xd

--- static/src/views/hide_icon_delete.js
+++ static/src/views/hide_icon_delete.js
@@ -1,16 +1,17 @@
 /** @odoo-module **/
-import { patch } from "@web/core/utils/patch";
-import { ListRenderer } from "@web/views/list/list_renderer";
-import { X2ManyField } from "@web/views/fields/x2many/x2many_field";
+import {patch} from "@web/core/utils/patch";
+import {ListRenderer} from "@web/views/list/list_renderer";
+import {X2ManyField} from "@web/views/fields/x2many/x2many_field";
 import session from "web.session";
 // import {listView} from "@web/views/list/list_view"
 // import {ListController} from "@web/views/list/list_controller";
-import { registry } from "@web/core/registry";
-import { evaluateExpr } from "@web/core/py_js/py";
-import { TestX2ManyField } from "./one2many_selectable";
+import {registry} from "@web/core/registry";
+import {evaluateExpr} from "@web/core/py_js/py";
+import {TestX2ManyField} from "./one2many_selectable";
 import rpc from "web.rpc";
+import {SplitViewRenderer} from "./split_view/list_renderer";
 
-const { onWillStart, onWillUpdateProps, useState } = owl;
+const {onWillStart, onWillUpdateProps, useState} = owl;
 // const {Component, onWillStart, onRendered, onMounted} = owl;
 
 // patch(ListRenderer.prototype, "my_list_view_patch", {
@@ -41,284 +42,275 @@ const { onWillStart, onWillUpdateProps, useState } = owl;
 // registry.category("fields").add("my_list_renderer", MyListRender);
 
 export class TestListRenderer extends ListRenderer {
-  setup() {
-    super.setup();
-    // session.user_has_group('gastos_tqc.res_groups_aprobador_gastos').then(function (has_group) {
-    //     console.log("entro xd")
-    //     if (has_group) {
-    //
-    //     }
-    // });
-    this.showRemoveIcon = useState({ value: false });
-    onWillStart(() => this._loadPro());
-  }
-
-  // Cambiar clase de tabla
-  getRowClass(record) {
-    // classnames coming from decorations
-    const classNames = this.props.archInfo.decorations
-      .filter((decoration) =>
-        evaluateExpr(decoration.condition, record.evalContext)
-      )
-      .map((decoration) => decoration.class);
-    if (record.selected) {
-      classNames.push("table_info_new");
-    }
-    // "o_selected_row" classname for the potential row in edition
-    if (record.isInEdition) {
-      classNames.push("o_selected_row");
-    }
-    if (record.selected) {
-      classNames.push("o_data_row_selected");
-    }
-    if (this.canResequenceRows) {
-      classNames.push("o_row_draggable");
-    }
-    return classNames.join(" ");
-  }
-
-  get hasSelectors() {
-    const dataRecord = this.props.list.model.root.data;
-    if (
-      (dataRecord.state === "jefatura" &&
-        dataRecord.uid_create === 3 &&
-        dataRecord.current_user === 1) ||
-      (dataRecord.state === "contable" && dataRecord.uid_create === 2)
-    ) {
-      this.props.allowSelectors = true;
-      let list = this.props.list;
-      list.selection = list.records.filter((rec) => rec.selected);
-      list.selectDomain = (value) => {
-        list.isDomainSelected = value;
-        list.model.notify();
-      };
+    setup() {
+        super.setup();
+        // session.user_has_group('gastos_tqc.res_groups_aprobador_gastos').then(function (has_group) {
+        //     console.log("entro xd")
+        //     if (has_group) {
+        //
+        //     }
+        // });
+        this.showRemoveIcon = useState({value: false});
+        onWillStart(() => this._loadPro());
     }
 
-    return this.props.allowSelectors && !this.env.isSmall;
-  }
+    // Cambiar clase de tabla
+    toggleRecordSelection(record) {
+        console.log("toggleRecordSelection 2.0")
+        this.props.list.unselectRecord(true);
 
-  async _loadPro() {
-    // const dataRecord = this.props.list.model.root.data
-    // console.log("dataRecord state ", dataRecord.state)
-    // if (dataRecord.state === 'jefatura'){
-    //     this.showObservationButton.value = false
-    // }
-    if (await session.user_has_group("gastos_tqc.res_groups_administrator")) {
-      this.showRemoveIcon.value = false;
+        // if (!this.canSelectRecord) {
+        //     console.log("toggleRecordSelection 2.1 ", this.canSelectRecord)
+        //     return;
+        // }
+        record.toggleSelection();
+        this.props.list.selectDomain(false);
     }
-    if (
-      await session.user_has_group("gastos_tqc.res_groups_aprobador_gastos")
-    ) {
-      this.showRemoveIcon.value = false;
+
+    get hasSelectors() {
+        const dataRecord = this.props.list.model.root.data;
+        if (
+            (dataRecord.state === "jefatura" &&
+                dataRecord.uid_create === 3 &&
+                dataRecord.current_user === 1) ||
+            (dataRecord.state === "contable" && dataRecord.uid_create === 2)
+        ) {
+            this.props.allowSelectors = true;
+            let list = this.props.list;
+            list.selection = list.records.filter((rec) => rec.selected);
+            list.selectDomain = (value) => {
+                list.isDomainSelected = value;
+                list.model.notify();
+            };
+        }
+
+        return this.props.allowSelectors && !this.env.isSmall;
     }
-  }
+
+    async _loadPro() {
+        // const dataRecord = this.props.list.model.root.data
+        // console.log("dataRecord state ", dataRecord.state)
+        // if (dataRecord.state === 'jefatura'){
+        //     this.showObservationButton.value = false
+        // }
+        if (await session.user_has_group("gastos_tqc.res_groups_administrator")) {
+            this.showRemoveIcon.value = false;
+        }
+        if (
+            await session.user_has_group("gastos_tqc.res_groups_aprobador_gastos")
+        ) {
+            this.showRemoveIcon.value = false;
+        }
+    }
 }
 
+// TestListRenderer.template = 'one2many_mass_select_delete.ListRenderer';
 TestListRenderer.recordRowTemplate = "owl_learn.ClickMe.RecordRow";
 
+
 export class NewListRenderer extends X2ManyField {
-  setup() {
-    super.setup();
-    this.showModalDescription = useState({ value: false });
-    this.descriptionRechazo = useState({ text: "" });
-    this.showModalObserva = useState({ value: false });
-    this.descriptionObserva = useState({ text: "" });
-    this.showObservationButton = useState({ value: true });
-    onWillStart(() => this._loadPro());
-  }
-
-  async _loadPro() {
-    const dataRecord = this.props.record.data;
-    console.log("dataRecord state ", dataRecord.state);
-    if (dataRecord.state === "jefatura") {
-      this.showObservationButton.value = false;
+    setup() {
+        super.setup();
+        this.showModalDescription = useState({value: false});
+        this.descriptionRechazo = useState({text: ""});
+        this.showModalObserva = useState({value: false});
+        this.descriptionObserva = useState({text: ""});
+        this.showObservationButton = useState({value: true});
+        onWillStart(() => this._loadPro());
     }
-  }
 
-  get hasSelected() {
-    return this.list.records.filter((rec) => rec.selected).length;
-  }
-
-  async deleteSelected() {
-    // Obtener valor de campo state del registro padre
-    this.descriptionRechazo.text = "";
-    this.showModalDescription.value = true;
-  }
-
-  async observationSelected() {
-    this.descriptionObserva.text = "";
-    this.showModalObserva.value = true;
-  }
-
-  async _onClickAceptar() {
-    const self = this;
-    console.log("entro nuevo eliminado x");
-    // self.rendererProps.list.model.load();
-    // // Obtener valor de campo state del registro padre
-    // const saveButton = document.querySelector(".o_form_button_save");
-    // //
-    // // // Verifica si el botón existe para evitar errores
-    // if (saveButton) {
-    //   console.log("entro sabebutoin");
-    //   // Dispara el evento de clic en el botón
-    //   saveButton.click();
-    // }
-
-    const state = this.props.record.data.state;
-    let current_model = this.field.relation;
-    let selected = this.list.records.filter((rec) => rec.selected);
-    this.list.records;
-    let selected_list = [];
-    selected.forEach((rec) => {
-      if (rec.data.id) {
-        selected_list.push(parseInt(rec.data.id));
-      } else {
-        if (this.activeActions.onDelete) {
-          selected.forEach((rec) => {
-            this.activeActions.onDelete(rec);
-          });
+    async _loadPro() {
+        const dataRecord = this.props.record.data;
+        console.log("dataRecord state ", dataRecord.state);
+        if (dataRecord.state === "jefatura") {
+            this.showObservationButton.value = false;
         }
-      }
-    });
-    if (selected_list.length !== 0) {
-      // this.showModalDescription.value = true
-      await rpc.query({
-        model: current_model,
-        method: "write",
-        args: [
-          selected_list,
-          {
-            revisado_state: "rechazado_" + state,
-            [state === "contable"
-              ? "observacioncontabilidad"
-              : "observacionjefatura"]: this.descriptionRechazo.text,
-          },
-        ],
-      });
-      // Guardar el formulario principal
-      const saveButton = document.querySelector(".o_form_button_save");
-      if (saveButton) {
-        saveButton.click();
-      }
-
-      self.rendererProps.list.model.load();
     }
-    this.showModalDescription.value = false;
-  }
 
-  async _onClickAceptarObserva() {
-    const self = this;
+    get hasSelected() {
+        return this.list.records.filter((rec) => rec.selected).length;
+    }
 
-    self.rendererProps.list.model.load();
-    // // Obtener valor de campo state del registro padre
-    // const saveButton = document.querySelector('.o_form_button_save');
-    //
-    // // Verifica si el botón existe para evitar errores
-    // if (saveButton) {
-    //     // Dispara el evento de clic en el botón
-    //     saveButton.click();
-    // }
+    async deleteSelected() {
+        // Obtener valor de campo state del registro padre
+        this.descriptionRechazo.text = "";
+        this.showModalDescription.value = true;
+    }
 
-    const state = this.props.record.data.state;
-    // Obtener valor de campo state del registro padre
-    var current_model = this.field.relation;
-    let selected = this.list.records.filter((rec) => rec.selected);
-    this.list.records;
-    var selected_list = [];
-    selected.forEach((rec) => {
-      if (rec.data.id) {
-        selected_list.push(parseInt(rec.data.id));
-      } else {
-        if (this.activeActions.onDelete) {
-          selected.forEach((rec) => {
-            this.activeActions.onDelete(rec);
-          });
+    async observationSelected() {
+        this.descriptionObserva.text = "";
+        this.showModalObserva.value = true;
+    }
+
+    async _onClickAceptar() {
+        const self = this;
+        console.log("entro nuevo eliminado x");
+        // self.rendererProps.list.model.load();
+        // // Obtener valor de campo state del registro padre
+        // const saveButton = document.querySelector(".o_form_button_save");
+        // //
+        // // // Verifica si el botón existe para evitar errores
+        // if (saveButton) {
+        //   console.log("entro sabebutoin");
+        //   // Dispara el evento de clic en el botón
+        //   saveButton.click();
+        // }
+
+        const state = this.props.record.data.state;
+        let current_model = this.field.relation;
+        let selected = this.list.records.filter((rec) => rec.selected);
+        this.list.records;
+        let selected_list = [];
+        selected.forEach((rec) => {
+            if (rec.data.id) {
+                selected_list.push(parseInt(rec.data.id));
+            } else {
+                if (this.activeActions.onDelete) {
+                    selected.forEach((rec) => {
+                        this.activeActions.onDelete(rec);
+                    });
+                }
+            }
+        });
+        if (selected_list.length !== 0) {
+            // this.showModalDescription.value = true
+            await rpc.query({
+                model: current_model,
+                method: "write",
+                args: [
+                    selected_list,
+                    {
+                        revisado_state: "rechazado_" + state,
+                        [state === "contable"
+                            ? "observacioncontabilidad"
+                            : "observacionjefatura"]: this.descriptionRechazo.text,
+                    },
+                ],
+            });
+            // Guardar el formulario principal
+            const saveButton = document.querySelector(".o_form_button_save");
+            if (saveButton) {
+                saveButton.click();
+            }
+
+            self.rendererProps.list.model.load();
         }
-      }
-    });
-    if (selected_list.length !== 0) {
-      // this.showModalDescription.value = true
-      await rpc.query({
-        model: current_model,
-        method: "write",
-        args: [
-          selected_list,
-          {
-            revisado_state: "observado_" + state,
-            [state === "contable"
-              ? "observacioncontabilidad"
-              : "observacionjefatura"]: this.descriptionObserva.text,
-          },
-        ],
-      });
-
-      // Guardar el formulario principal
-      const saveButton = document.querySelector(".o_form_button_save");
-      if (saveButton) {
-        saveButton.click();
-      }
+        this.showModalDescription.value = false;
     }
-    this.showModalObserva.value = false;
-    // buscar el boton por clase o_form_button_save  y hacer click
-  }
 
-  async restaurarRecord() {
-    const self = this;
-    console.log("entro restaurarRecord");
-    
-    self.rendererProps.list.model.load();
-    // // Obtener valor de campo state del registro padre
-    // const saveButton = document.querySelector('.o_form_button_save');
-    //
-    // // Verifica si el botón existe para evitar errores
-    // if (saveButton) {
-    //     // Dispara el evento de clic en el botón
-    //     saveButton.click();
-    // }
-    const state = this.props.record.data.state;
-    var current_model = this.field.relation;
-    let selected = this.list.records.filter((rec) => rec.selected);
-    this.list.records;
-    var selected_list = [];
-    selected.forEach((rec) => {
-      if (rec.data.id) {
-        selected_list.push(parseInt(rec.data.id));
-      } else {
-        if (this.activeActions.onDelete) {
-          selected.forEach((rec) => {
-            this.activeActions.onDelete(rec);
-          });
+    async _onClickAceptarObserva() {
+        const self = this;
+
+        self.rendererProps.list.model.load();
+        // // Obtener valor de campo state del registro padre
+        // const saveButton = document.querySelector('.o_form_button_save');
+        //
+        // // Verifica si el botón existe para evitar errores
+        // if (saveButton) {
+        //     // Dispara el evento de clic en el botón
+        //     saveButton.click();
+        // }
+
+        const state = this.props.record.data.state;
+        // Obtener valor de campo state del registro padre
+        var current_model = this.field.relation;
+        let selected = this.list.records.filter((rec) => rec.selected);
+        this.list.records;
+        var selected_list = [];
+        selected.forEach((rec) => {
+            if (rec.data.id) {
+                selected_list.push(parseInt(rec.data.id));
+            } else {
+                if (this.activeActions.onDelete) {
+                    selected.forEach((rec) => {
+                        this.activeActions.onDelete(rec);
+                    });
+                }
+            }
+        });
+        if (selected_list.length !== 0) {
+            // this.showModalDescription.value = true
+            await rpc.query({
+                model: current_model,
+                method: "write",
+                args: [
+                    selected_list,
+                    {
+                        revisado_state: "observado_" + state,
+                        [state === "contable"
+                            ? "observacioncontabilidad"
+                            : "observacionjefatura"]: this.descriptionObserva.text,
+                    },
+                ],
+            });
+
+            // Guardar el formulario principal
+            const saveButton = document.querySelector(".o_form_button_save");
+            if (saveButton) {
+                saveButton.click();
+            }
         }
-      }
-    });
-    if (selected_list.length !== 0) {
-      // this.showModalDescription.value = true
-      await rpc.query({
-        model: current_model,
-        method: "write",
-        args: [
-          selected_list,
-          {
-            revisado_state: "borrador",
-          },
-        ],
-      });
-      // Guardar el formulario principal
-      const saveButton = document.querySelector(".o_form_button_save");
-      if (saveButton) {
-        saveButton.click();
-      }
+        this.showModalObserva.value = false;
+        // buscar el boton por clase o_form_button_save  y hacer click
     }
-    self.rendererProps.list.model.load();
-  }
 
-  _onClickClose() {
-    this.showModalDescription.value = false;
-  }
+    async restaurarRecord() {
+        const self = this;
+        console.log("entro restaurarRecord");
 
-  _onClickCloseObserva() {
-    this.showModalObserva.value = false;
-  }
+        self.rendererProps.list.model.load();
+        // // Obtener valor de campo state del registro padre
+        // const saveButton = document.querySelector('.o_form_button_save');
+        //
+        // // Verifica si el botón existe para evitar errores
+        // if (saveButton) {
+        //     // Dispara el evento de clic en el botón
+        //     saveButton.click();
+        // }
+        const state = this.props.record.data.state;
+        var current_model = this.field.relation;
+        let selected = this.list.records.filter((rec) => rec.selected);
+        this.list.records;
+        var selected_list = [];
+        selected.forEach((rec) => {
+            if (rec.data.id) {
+                selected_list.push(parseInt(rec.data.id));
+            } else {
+                if (this.activeActions.onDelete) {
+                    selected.forEach((rec) => {
+                        this.activeActions.onDelete(rec);
+                    });
+                }
+            }
+        });
+        if (selected_list.length !== 0) {
+            // this.showModalDescription.value = true
+            await rpc.query({
+                model: current_model,
+                method: "write",
+                args: [
+                    selected_list,
+                    {
+                        revisado_state: "borrador",
+                    },
+                ],
+            });
+            // Guardar el formulario principal
+            const saveButton = document.querySelector(".o_form_button_save");
+            if (saveButton) {
+                saveButton.click();
+            }
+        }
+        self.rendererProps.list.model.load();
+    }
+
+    _onClickClose() {
+        this.showModalDescription.value = false;
+    }
+
+    _onClickCloseObserva() {
+        this.showModalObserva.value = false;
+    }
 }
 
 // export const newListView = {
@@ -326,8 +318,8 @@ export class NewListRenderer extends X2ManyField {
 //     Renderer: NewListRenderer,
 // }
 NewListRenderer.components = {
-  ...X2ManyField.components,
-  ListRenderer: TestListRenderer,
+    ...X2ManyField.components,
+    ListRenderer: TestListRenderer,
 };
 NewListRenderer.template = "one2many_mass_select_delete.One2manyDelete";
 // newListView.Renderer.recordRowTemplate = 'owl_learn.ClickMe.RecordRow';
